fc84db9bae15532c0ec899f116168a58f15208d5
Darren Ferguson <darren102@gmail.com>
2018-04-05 15:13:09 -0400

    Add parent-child relationship

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,49 +1,76 @@
+/// Copyright (c) 2018 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
 import FluentPostgreSQL
 import Vapor
 
+/// Called before your application initializes.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
 public func configure(
-    _ config: inout Config,
-    _ env: inout Environment,
-    _ services: inout Services
-    ) throws {
-    // Step 2
-    try services.register(FluentPostgreSQLProvider())
+  _ config: inout Config,
+  _ env: inout Environment,
+  _ services: inout Services
+) throws {
+  // Register providers first
+  try services.register(FluentPostgreSQLProvider())
 
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
+  // Register routes to the router
+  let router = EngineRouter.default()
+  try routes(router)
+  services.register(router, as: Router.self)
 
-    var middlewares = MiddlewareConfig()
-    middlewares.use(DateMiddleware.self)
-    middlewares.use(ErrorMiddleware.self)
-    services.register(middlewares)
+  // Configure a database
+  var databases = DatabaseConfig()
+  let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+  let username = Environment.get("DATABASE_USER") ?? "vapor"
+  let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+  let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+  let databaseConfig = PostgreSQLDatabaseConfig(
+    hostname: hostname,
+    username: username,
+    database: databaseName,
+    password: password)
+  let database = PostgreSQLDatabase(config: databaseConfig)
+  databases.add(database: database, as: .psql)
+  services.register(databases)
 
-    // Configure a database
-    var databases = DatabaseConfig()
-    // 2
-    let hostname = Environment.get("DATABASE_HOSTNAME")
-        ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD")
-        ?? "password"
-    // 3
-    let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: hostname,
-        port: 5432,
-        username: username,
-        database: databaseName,
-        password: password)
-    // 4
-    let database = PostgreSQLDatabase(config: databaseConfig)
-    // 5
-    databases.add(database: database, as: .psql)
-    // 6
-    services.register(databases)
+  // Configure migrations
+  var migrations = MigrationConfig()
+  migrations.add(model: User.self, database: .psql)
+  migrations.add(model: Acronym.self, database: .psql)
+  services.register(migrations)
 
-    var migrations = MigrationConfig()
-    // Step 5
-    migrations.add(model: Acronym.self, database: .psql)
-    services.register(migrations)
+  // Configure the rest of your application here
+  // 1
+  var commandConfig = CommandConfig.default()
+  // 2
+  commandConfig.use(RevertCommand.self, as: "revert")
+  // 3
+  services.register(commandConfig)
 }
-
