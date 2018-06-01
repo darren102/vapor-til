a52dab150431ef97e7703549564bb1e009984f36
Darren Ferguson <darren102@gmail.com>
2018-06-01 12:10:55 +0100

    Add API authentication

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -28,51 +28,79 @@
 
 import Vapor
 import Fluent
+import Authentication
 
 struct AcronymsController: RouteCollection {
   func boot(router: Router) throws {
     let acronymsRoutes = router.grouped("api", "acronyms")
     acronymsRoutes.get(use: getAllHandler)
-    acronymsRoutes.post(Acronym.self, use: createHandler)
     acronymsRoutes.get(Acronym.parameter, use: getHandler)
-    acronymsRoutes.put(Acronym.parameter, use: updateHandler)
-    acronymsRoutes.delete(Acronym.parameter, use: deleteHandler)
     acronymsRoutes.get("search", use: searchHandler)
     acronymsRoutes.get("first", use: getFirstHandler)
     acronymsRoutes.get("sorted", use: sortedHandler)
     acronymsRoutes.get(Acronym.parameter, "user", use: getUserHandler)
-    acronymsRoutes.post(Acronym.parameter,
-                        "categories",
-                        Category.parameter,
-                        use: addCategoriesHandler)
     acronymsRoutes.get(Acronym.parameter,
                        "categories",
                        use: getCategoriesHandler)
+
+    // 1
+    let tokenAuthMiddleware = User.tokenAuthMiddleware()
+    let guardAuthMiddleware = User.guardAuthMiddleware()
+    // 2
+    let tokenAuthGroup = acronymsRoutes.grouped(
+        tokenAuthMiddleware,
+        guardAuthMiddleware)
+    // 3
+    tokenAuthGroup.post(AcronymCreateData.self, use: createHandler)
+    tokenAuthGroup.delete(Acronym.parameter, use: deleteHandler)
+    tokenAuthGroup.put(Acronym.parameter, use: updateHandler)
+    tokenAuthGroup.post(
+        Acronym.parameter,
+        "categories",
+        Category.parameter,
+        use: addCategoriesHandler)
+
   }
 
   func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
     return Acronym.query(on: req).all()
   }
 
-  func createHandler(_ req: Request, acronym: Acronym) throws -> Future<Acronym> {
-    return acronym.save(on: req)
-  }
+    // 1
+    func createHandler(
+        _ req: Request,
+        data: AcronymCreateData) throws -> Future<Acronym> {
+        // 2
+        let user = try req.requireAuthenticated(User.self)
+        // 3
+        let acronym = try Acronym(
+            short: data.short, long: data.long,
+            userID: user.requireID())
+        // 4
+        return acronym.save(on: req)
+    }
+
 
   func getHandler(_ req: Request) throws -> Future<Acronym> {
     return try req.parameters.next(Acronym.self)
   }
 
-  func updateHandler(_ req: Request) throws -> Future<Acronym> {
-    return try flatMap(to: Acronym.self,
-                       req.parameters.next(Acronym.self),
-                       req.content.decode(Acronym.self)) {
-                        acronym, updatedAcronym in
-                        acronym.short = updatedAcronym.short
-                        acronym.long = updatedAcronym.long
-                        acronym.userID = updatedAcronym.userID
-                        return acronym.save(on: req)
+    func updateHandler(_ req: Request) throws -> Future<Acronym> {
+        // 1
+        return try flatMap(
+            to: Acronym.self,
+            req.parameters.next(Acronym.self),
+            req.content.decode(AcronymCreateData.self)
+        ) { acronym, updateData in
+            acronym.short = updateData.short
+            acronym.long = updateData.long
+            // 2
+            let user = try req.requireAuthenticated(User.self)
+            acronym.userID = try user.requireID()
+            return acronym.save(on: req)
+        }
+
     }
-  }
 
   func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
     return try req.parameters.next(Acronym.self).delete(on: req).transform(to: HTTPStatus.noContent)
@@ -102,12 +130,12 @@ struct AcronymsController: RouteCollection {
   }
 
   // 1
-  func getUserHandler(_ req: Request) throws -> Future<User> {
+  func getUserHandler(_ req: Request) throws -> Future<User.Public> {
     // 2
     return try req.parameters.next(Acronym.self)
-      .flatMap(to: User.self) { acronym in
+      .flatMap(to: User.Public.self) { acronym in
         // 3
-        try acronym.user.get(on: req)
+        try acronym.user.get(on: req).convertToPublic()
     }
   }
 
@@ -136,4 +164,9 @@ struct AcronymsController: RouteCollection {
                 try acronym.categories.query(on: req).all()
         }
     }
+}
+
+struct AcronymCreateData: Content {
+    let short: String
+    let long: String
 }
---
+++ Tests/AppTests/Models+Testable.swift
@@ -0,0 +1,48 @@
+@testable import App
+import FluentPostgreSQL
+import Crypto
+
+extension User {
+    // 1
+    static func create(
+        name: String = "Luke",
+        username: String? = nil,
+        on connection: PostgreSQLConnection
+        ) throws -> User {
+        var createUsername: String
+        // 2
+        if let suppliedUsername = username {
+            createUsername = suppliedUsername
+            // 3
+        } else {
+            createUsername = UUID().uuidString
+        }
+        // 4
+        let password = try BCrypt.hash("password")
+        let user = User(
+            name: name,
+            username: createUsername,
+            password: password)
+        return try user.save(on: connection).wait()
+    }
+}
+
+extension Acronym {
+  static func create(short: String = "TIL", long: String = "Today I Learned", user: User? = nil, on connection: PostgreSQLConnection) throws -> Acronym {
+    var acronymsUser = user
+
+    if acronymsUser == nil {
+      acronymsUser = try User.create(on: connection)
+    }
+
+    let acronym = Acronym(short: short, long: long, userID: acronymsUser!.id!)
+    return try acronym.save(on: connection).wait()
+  }
+}
+
+extension App.Category {
+  static func create(name: String = "Random", on connection: PostgreSQLConnection) throws -> App.Category {
+    let category = Category(name: name)
+    return try category.save(on: connection).wait()
+  }
+}
